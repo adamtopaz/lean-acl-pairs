c1e85aafefde58f3eb11cf2915299136e2ac4872
Adam Topaz <github@adamtopaz.com>
2022-04-15 08:30:23 -0600

    bump

--- src/for_mathlib/valuation_subring/basic.lean
+++ src/for_mathlib/valuation_subring/basic.lean
@@ -2,25 +2,11 @@ import ring_theory.subring.basic
 import mul_subgroup.adjoin
 import ring_theory.ideal.local_ring
 import ring_theory.integral_domain
-
-structure valuation_subring (K : Type*) [field K] extends subring K :=
-(mem_or_inv_mem' : ∀ x : K, x ∈ carrier ∨ x⁻¹ ∈ carrier)
+import ring_theory.valuation.valuation_subring
 
 namespace valuation_subring
 
 variables {K : Type*} [field K]
-
-instance : set_like (valuation_subring K) K := 
-{ coe := λ R, R.carrier,
-  coe_injective' := begin
-    intros R S h,
-    cases R with R _, cases S with S _, 
-    congr,
-    ext x,
-    dsimp at h,
-    change (x ∈ R.carrier ↔ x ∈ S.carrier),
-    rw h,
-  end }
 
 lemma one_mem (R : valuation_subring K) : (1 : K) ∈ R := 
 R.to_subring.one_mem
@@ -38,16 +24,6 @@ R.to_subring.sub_mem hx hy
 lemma mul_mem (R : valuation_subring K) {x y : K} (hx : x ∈ R) (hy : y ∈ R) : 
   x * y ∈ R := 
 R.to_subring.mul_mem hx hy
-
-lemma mem_or_inv_mem (R : valuation_subring K) (x : K) : 
-  x ∈ R ∨ x⁻¹ ∈ R := 
-R.mem_or_inv_mem' x
-
-instance (R : valuation_subring K) : comm_ring R := 
-show comm_ring R.to_subring, by apply_instance
-
-instance (R : valuation_subring K) : is_domain R :=
-show is_domain R.to_subring, by apply_instance
 
 def units (R : valuation_subring K) : set Kˣ := 
 { u | (u : K) ∈ R ∧ (u⁻¹ : K) ∈ R }
