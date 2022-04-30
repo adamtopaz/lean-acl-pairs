29a39dc94f1973840647258777627c50d2cc3ab8
Adam Topaz <github@adamtopaz.com>
2022-04-29 19:48:50 -0600

    bump mathlib

--- src/for_mathlib/valuation_subring/basic.lean
+++ src/for_mathlib/valuation_subring/basic.lean
@@ -8,22 +8,9 @@ namespace valuation_subring
 
 variables {K : Type*} [field K]
 
-lemma one_mem (R : valuation_subring K) : (1 : K) ∈ R := 
-R.to_subring.one_mem
-
-lemma zero_mem (R : valuation_subring K) : (0 : K) ∈ R := 
-R.to_subring.zero_mem
-
-lemma neg_mem (R : valuation_subring K) {x : K} (hx : x ∈ R) : -x ∈ R := 
-R.to_subring.neg_mem hx
-
 lemma sub_mem (R : valuation_subring K) {x y : K} (hx : x ∈ R) (hy : y ∈ R) : 
   x - y ∈ R := 
 R.to_subring.sub_mem hx hy
-
-lemma mul_mem (R : valuation_subring K) {x y : K} (hx : x ∈ R) (hy : y ∈ R) : 
-  x * y ∈ R := 
-R.to_subring.mul_mem hx hy
 
 def units (R : valuation_subring K) : set Kˣ := 
 { u | (u : K) ∈ R ∧ (u⁻¹ : K) ∈ R }
@@ -31,7 +18,7 @@ def units (R : valuation_subring K) : set Kˣ :=
 lemma neg_mem_units (R : valuation_subring K) {x : Kˣ} (hx : x ∈ R.units) : 
   -x ∈ R.units := 
 begin
-  split, convert R.neg_mem hx.1, convert R.neg_mem hx.2, field_simp,
+  split, convert R.neg_mem _ hx.1, convert R.neg_mem _ hx.2, field_simp,
 end
 
 lemma one_mem_units (R : valuation_subring K) : (1 : Kˣ) ∈ R.units := 
@@ -45,8 +32,8 @@ lemma mul_mem_units (R : valuation_subring K) {x y : Kˣ}
   (hx : x ∈ R.units) (hy : y ∈ R.units) :
   x * y ∈ R.units := 
 begin
-  split, push_cast, apply R.mul_mem hx.1 hy.1,
-  simp only [units.coe_mul], rw mul_inv₀, apply R.mul_mem hx.2 hy.2, 
+  split, push_cast, apply R.mul_mem _ _ hx.1 hy.1,
+  simp only [units.coe_mul], rw mul_inv₀, apply R.mul_mem _ _ hx.2 hy.2, 
 end
 
 def nonunits (R : valuation_subring K) : set K :=
