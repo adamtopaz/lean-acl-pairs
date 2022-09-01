a755b6aebca6ecbf5c048a598dce69b06b8ce38f
Adam Topaz <github@adamtopaz.com>
2022-08-31 20:35:19 -0600

    kill remaiining sorries

--- src/for_mathlib/valuation_subring/basic.lean
+++ src/for_mathlib/valuation_subring/basic.lean
@@ -27,13 +27,33 @@ begin
   { rintros (h : R.valuation u = 1), split, 
     rw ← valuation_subring.valuation_le_one_iff, apply le_of_eq, assumption,
     rw ← valuation_subring.valuation_le_one_iff, apply le_of_eq, simpa },
-  { intros hh, sorry }
+  { rintros ⟨h1,h2⟩, change R.valuation u = 1,
+    rw ← valuation_subring.valuation_le_one_iff at h1 h2,
+    simp only [map_inv₀] at h2,
+    rw inv_le_one₀ at h2,
+    apply le_antisymm; assumption,
+    intro c, simpa using c }
 end
 
 lemma mem_nonunits_iff_mem_and_nmem (R : valuation_subring K) (x : K) : 
-  x ∈ R.nonunits ↔ x ∈ R ∧ x ∉ (coe : Kˣ → K) '' R.unit_group := sorry
+  x ∈ R.nonunits ↔ x ∈ R ∧ x ∉ (coe : Kˣ → K) '' R.unit_group := 
+begin
+  dsimp [valuation_subring.nonunits],
+  split,
+  { intro h, split,
+    rw ← valuation_subring.valuation_le_one_iff, apply le_of_lt, assumption,
+    push_neg, rintros u (hu : R.valuation u = 1) (rfl : _ = _),
+    exact ne_of_lt h hu },
+  { rintro ⟨h1,h2⟩, 
+    push_neg at h2,
+    rw ← valuation_subring.valuation_le_one_iff at h1,
+    by_cases hh : (R.valuation x = 1),
+    { exfalso, apply h2 (units.mk0 x _) hh rfl, intro c, 
+      simpa [c] using hh },
+    { exact lt_of_le_of_ne h1 hh } }
+end
 
 lemma mem_principal_unit_group_iff_mem (R : valuation_subring K) (x : Kˣ) :
-  x ∈ R.principal_unit_group ↔ (x : K) - 1 ∈ R.nonunits := sorry
+  x ∈ R.principal_unit_group ↔ (x : K) - 1 ∈ R.nonunits := iff.refl _
 
 end valuation_subring
